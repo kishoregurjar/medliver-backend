209319a088a9c63d45e094115c95b14c3ede4677
Chetangit123 <pchetan839@gmail.com>
2025-05-13 10:16:10 +0530

    created API for get-all-medicines, get-medicine-by-id

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,32 +2,32 @@ const express = require("express");
 const indexController = require("../controllers/indexController");
 const { verifyUserToken } = require("../utils/jsonWebToken");
 const { uploadUserProfilePic, uploadPrescription } = require("../services/multer");
-const { validate, validateQuery, getAllSpecialOfferValidation, getAllFeatureProductValidation, getAllSellingProductValidation, createDoctorLeadValidation,registerCustomerSchema,CustomerverifyOtpSchema,loginCustomerSchema,forgetPasswordCustomerValidation,resetPasswordCustomerValidation,signUpSignInWithGoogleValidation,addAddressValidation,editAddressSchema,getOrDeleteCustomerAddress,addToCartSchema,changeQuantitySchema,removeItemFromCartSchema,applyInsuranceSchema,emergencyVehicleRequestSchema,updateUserProfileValidation,createOrderValidation,getOrderByIdValidation,createRazorpayOrderValidation,verifyRazorpayPaymentValidation, autoCompleteAddressValidation,getDistanceBetweenCoordsValidation,getRouteBetweenCoordsValidation,searchUMedicineValidation,logMedicineClickValidation} = require("../middleware/validation")
+const { validate, validateQuery, getAllSpecialOfferValidation, getAllFeatureProductValidation, getAllSellingProductValidation, createDoctorLeadValidation, registerCustomerSchema, CustomerverifyOtpSchema, loginCustomerSchema, forgetPasswordCustomerValidation, resetPasswordCustomerValidation, signUpSignInWithGoogleValidation, addAddressValidation, editAddressSchema, getOrDeleteCustomerAddress, addToCartSchema, changeQuantitySchema, removeItemFromCartSchema, applyInsuranceSchema, emergencyVehicleRequestSchema, updateUserProfileValidation, createOrderValidation, getOrderByIdValidation, createRazorpayOrderValidation, verifyRazorpayPaymentValidation, autoCompleteAddressValidation, getDistanceBetweenCoordsValidation, getRouteBetweenCoordsValidation, searchUMedicineValidation, logMedicineClickValidation } = require("../middleware/validation")
 
 const router = express.Router();
 
 /** Auth Route */
 
 
-router.post("/register-user", validate(registerCustomerSchema),indexController.customerController.registerUser);
-router.post("/verify-otp", validate(CustomerverifyOtpSchema),indexController.customerController.verifyOtp);
-router.post("/user-login",validate(loginCustomerSchema), indexController.customerController.loginUser);
-router.post("/forget-password",validate(forgetPasswordCustomerValidation), indexController.customerController.forgetPassword);
-router.post("/verify-forget-password-otp",validate(CustomerverifyOtpSchema), indexController.customerController.verifyForgetPasswordOtp);
-router.post("/reset-password", validate(resetPasswordCustomerValidation),indexController.customerController.resetPassword);
+router.post("/register-user", validate(registerCustomerSchema), indexController.customerController.registerUser);
+router.post("/verify-otp", validate(CustomerverifyOtpSchema), indexController.customerController.verifyOtp);
+router.post("/user-login", validate(loginCustomerSchema), indexController.customerController.loginUser);
+router.post("/forget-password", validate(forgetPasswordCustomerValidation), indexController.customerController.forgetPassword);
+router.post("/verify-forget-password-otp", validate(CustomerverifyOtpSchema), indexController.customerController.verifyForgetPasswordOtp);
+router.post("/reset-password", validate(resetPasswordCustomerValidation), indexController.customerController.resetPassword);
 router.get("/get-user-details", verifyUserToken(), indexController.customerController.getUserDetails);
 router.post("/change-password", verifyUserToken(), indexController.customerController.changeUserPassword);
-router.patch("/update-user-profile",validate(updateUserProfileValidation) ,verifyUserToken(), indexController.customerController.updateUserProfile);
+router.patch("/update-user-profile", validate(updateUserProfileValidation), verifyUserToken(), indexController.customerController.updateUserProfile);
 router.post("/update-user-profile-picture", verifyUserToken(), uploadUserProfilePic, indexController.customerController.updateUserProfilePicture);
-router.post("signup-signin-with-google", validate,indexController.customerController.signUPSignInWithGoogle);
+router.post("signup-signin-with-google", validate, indexController.customerController.signUPSignInWithGoogle);
 
 /** Address Routes */
-router.post("/add-address",validate(addAddressValidation), verifyUserToken(), indexController.customerController.addAddress);
+router.post("/add-address", validate(addAddressValidation), verifyUserToken(), indexController.customerController.addAddress);
 router.get("/get-all-address", verifyUserToken(), indexController.customerController.getAllAddress);
-router.put("/edit-address", validate(editAddressSchema),verifyUserToken(), indexController.customerController.editAddress);
-router.delete("/delete-address", validateQuery(getOrDeleteCustomerAddress),verifyUserToken(), indexController.customerController.deleteAddress);
-router.get('/get-address-by-id',validateQuery(getOrDeleteCustomerAddress), verifyUserToken(), indexController.customerController.getAddressById);
-router.put('/set-default-address', validate(getOrDeleteCustomerAddress),verifyUserToken(), indexController.customerController.setDefaultAddress);
+router.put("/edit-address", validate(editAddressSchema), verifyUserToken(), indexController.customerController.editAddress);
+router.delete("/delete-address", validateQuery(getOrDeleteCustomerAddress), verifyUserToken(), indexController.customerController.deleteAddress);
+router.get('/get-address-by-id', validateQuery(getOrDeleteCustomerAddress), verifyUserToken(), indexController.customerController.getAddressById);
+router.put('/set-default-address', validate(getOrDeleteCustomerAddress), verifyUserToken(), indexController.customerController.setDefaultAddress);
 
 /** Featue and selling and special offer Routes */
 
@@ -36,42 +36,50 @@ router.get("/get-all-top-selling-product", validateQuery(getAllSellingProductVal
 router.get("/get-all-special-offer", validateQuery(getAllSpecialOfferValidation), indexController.customerSpecialProductController.getallSpecialOffers)
 
 /** Cart Routes */
-router.post('/add-to-cart', validate(addToCartSchema),verifyUserToken(), indexController.customerCartController.addToCart);
+router.post('/add-to-cart', validate(addToCartSchema), verifyUserToken(), indexController.customerCartController.addToCart);
 router.get('/get-cart', verifyUserToken(), indexController.customerCartController.getCart);
-router.put('/change-cart-product-quantity',validate(changeQuantitySchema) ,verifyUserToken(), indexController.customerCartController.changeQuantity);
-router.put('/remove-item-from-cart',validate(removeItemFromCartSchema) ,verifyUserToken(), indexController.customerCartController.removeItemFromCart);
+router.put('/change-cart-product-quantity', validate(changeQuantitySchema), verifyUserToken(), indexController.customerCartController.changeQuantity);
+router.put('/remove-item-from-cart', validate(removeItemFromCartSchema), verifyUserToken(), indexController.customerCartController.removeItemFromCart);
 
 /** Insurance route */
-router.post("/apply-for-insurance",validate(applyInsuranceSchema),indexController.customerMissLiniesController.applyInsurance);
-router.post("/request-for-emergency-vehicle",validate(emergencyVehicleRequestSchema), indexController.customerMissLiniesController.requestEmergencyVehicle)
+router.post("/apply-for-insurance", validate(applyInsuranceSchema), indexController.customerMissLiniesController.applyInsurance);
+router.post("/request-for-emergency-vehicle", validate(emergencyVehicleRequestSchema), indexController.customerMissLiniesController.requestEmergencyVehicle)
 
 
 
 /** Order Routes */
-router.post("/create-order",validate(createOrderValidation), verifyUserToken(), indexController.customerOrderController.createOrder);
+router.post("/create-order", validate(createOrderValidation), verifyUserToken(), indexController.customerOrderController.createOrder);
 router.get("/get-all-orders", verifyUserToken(), indexController.customerOrderController.getAllOrders);
-router.get("/get-order-by-id",validateQuery(getOrderByIdValidation), verifyUserToken(), indexController.customerOrderController.getOrderById);
-router.put("/cancel-order", validateQuery(getOrderByIdValidation),verifyUserToken(), indexController.customerOrderController.cancleOrder);
+router.get("/get-order-by-id", validateQuery(getOrderByIdValidation), verifyUserToken(), indexController.customerOrderController.getOrderById);
+router.put("/cancel-order", validateQuery(getOrderByIdValidation), verifyUserToken(), indexController.customerOrderController.cancleOrder);
 
 /** Payment Routes */
 
-router.post('/create-payment-order', validate(createRazorpayOrderValidation),indexController.customerPaymentController.createOrder);
-router.post('/verify-payment', validate(verifyRazorpayPaymentValidation),indexController.customerPaymentController.verifyPayment);
+router.post('/create-payment-order', validate(createRazorpayOrderValidation), indexController.customerPaymentController.createOrder);
+router.post('/verify-payment', validate(verifyRazorpayPaymentValidation), indexController.customerPaymentController.verifyPayment);
 
 /** Prescription */
 
 router.post('/upload-prescription', verifyUserToken(), uploadPrescription, indexController.customerOrderController.uploadPrescription)
 
 /** Map Integration */
-router.post('/search-autocomplete-address',validate( autoCompleteAddressValidation), indexController.commonController.autoCompleteAddress);
-router.post('/get-distance-between-coords', validate(getDistanceBetweenCoordsValidation),indexController.commonController.getDistanceBetweenCoords);
-router.post('/get-routes-bw-coords', validate(getRouteBetweenCoordsValidation),indexController.commonController.getRouteBetweenCoords);
+router.post('/search-autocomplete-address', validate(autoCompleteAddressValidation), indexController.commonController.autoCompleteAddress);
+router.post('/get-distance-between-coords', validate(getDistanceBetweenCoordsValidation), indexController.commonController.getDistanceBetweenCoords);
+router.post('/get-routes-bw-coords', validate(getRouteBetweenCoordsValidation), indexController.commonController.getRouteBetweenCoords);
 
 /**Medicine */
-router.get('/search-medicine', validateQuery(searchUMedicineValidation),indexController.customerMedicineController.searchMedicine);
-router.post('/log-medicine-click',validate(logMedicineClickValidation) ,indexController.customerMedicineController.logMedicineClick);
+router.get('/search-medicine', validateQuery(searchUMedicineValidation), indexController.customerMedicineController.searchMedicine);
+router.get(
+    "/get-all-medicines",
+    indexController.medicineController.getAllMedicines
+);
+router.get(
+    "/get-medicine-by-id",
+    indexController.medicineController.getMedicineById
+);
+router.post('/log-medicine-click', validate(logMedicineClickValidation), indexController.customerMedicineController.logMedicineClick);
 router.get('/get-top-picks', indexController.customerMedicineController.getUserTopPicksWithSimilar)
 
 /**DoctoreLead Routes */
-router.post("/create-doctoreLead",validate(createDoctorLeadValidation),indexController.customerMissLiniesController.createDoctoreLead);
+router.post("/create-doctoreLead", validate(createDoctorLeadValidation), indexController.customerMissLiniesController.createDoctoreLead);
 module.exports = router;
