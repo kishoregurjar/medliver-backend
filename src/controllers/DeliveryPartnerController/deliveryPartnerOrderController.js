302ae7e3156f444c77193612d13c63112dab53a9
Chetangit123 <pchetan839@gmail.com>
2025-05-13 16:58:25 +0530

    changes in getRequestedOrder

--- src/controllers/DeliveryPartnerController/deliveryPartnerOrderController.js
+++ src/controllers/DeliveryPartnerController/deliveryPartnerOrderController.js
@@ -4,6 +4,6 @@ const asyncErrorHandler = require("../../utils/asyncErrorHandler");
 
 module.exports.getRequestedOrder = asyncErrorHandler(async (req, res, next) => {
     const deliveryPartnerId = req.partner._id;
-    const orders = await ordersModel.find({ deliveryPartnerId: deliveryPartnerId })
+    const orders = await ordersModel.find({ deliveryPartnerId: deliveryPartnerId }).populate('assignedPharmacyId')
     return successRes(res, 200, true, "Orders fetched successfully", orders);
 })
