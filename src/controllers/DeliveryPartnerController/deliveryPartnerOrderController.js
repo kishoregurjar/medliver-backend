0dc21fb2016e50111821cdca8a38e8a6a756276f
tirolenitin <tirolenitin9926@gmail.com>
2025-05-19 14:10:31 +0530

    create accept reject  api for delivery partner

--- src/controllers/DeliveryPartnerController/deliveryPartnerOrderController.js
+++ src/controllers/DeliveryPartnerController/deliveryPartnerOrderController.js
@@ -1,24 +1,198 @@
+const notificationModel = require("../../modals/notification.model");
 const ordersModel = require("../../modals/orders.model");
 const { successRes } = require("../../services/response");
 const asyncErrorHandler = require("../../utils/asyncErrorHandler");
 const CustomError = require("../../utils/customError");
+const { sendExpoNotification } = require("../../utils/expoNotification");
+const { getDistance } = require("../../utils/helper");
+const DeliveryPartner = require("../../modals/delivery.model");
+const customerModel = require("../../modals/customer.model");
 
 module.exports.getRequestedOrder = asyncErrorHandler(async (req, res, next) => {
-    const deliveryPartnerId = req.partner._id;
-    const orders = await ordersModel.find({ deliveryPartnerId: deliveryPartnerId }).populate('assignedPharmacyId')
-    return successRes(res, 200, true, "Orders fetched successfully", orders);
-})
+  const deliveryPartnerId = req.partner._id;
+  const orders = await ordersModel
+    .find({ deliveryPartnerId: deliveryPartnerId })
+    .populate("assignedPharmacyId");
+  return successRes(res, 200, true, "Orders fetched successfully", orders);
+});
 
 module.exports.getOrderById = asyncErrorHandler(async (req, res, next) => {
-    const deliveryPartnerId = req.partner._id;
-    console.log("deliveryPartnerId", deliveryPartnerId);
-    let { orderId } = req.query;
-    if (!orderId) {
-        return next(new CustomError("Order ID is required", 400));
+  const deliveryPartnerId = req.partner._id;
+  console.log("deliveryPartnerId", deliveryPartnerId);
+  let { orderId } = req.query;
+  if (!orderId) {
+    return next(new CustomError("Order ID is required", 400));
+  }
+  const order = await ordersModel
+    .findOne({ _id: orderId, deliveryPartnerId: deliveryPartnerId })
+    .select(
+      "-orderStatus -pharmacyQueue -deliveryPartnerQueue -pharmacyResponseStatus -pharmacyAttempts -deliveryPartnerAttempts"
+    )
+    .populate(
+      "assignedPharmacyId",
+      "pharmacyName address phone completeAddress"
+    )
+    .populate("customerId", "fullName phoneNumber profilePicture");
+  if (!order) {
+    return next(new CustomError("Order not found", 404));
+  }
+  return successRes(res, 200, true, "Order fetched successfully", order);
+});
+
+module.exports.acceptRejectOrder = asyncErrorHandler(async (req, res, next) => {
+  const deliveryPartnerId = req.partner._id;
+  let { orderId, status } = req.body;
+  if (!orderId) {
+    return next(new CustomError("Order ID is required", 400));
+  }
+  const order = await ordersModel.findOne({
+    _id: orderId,
+    deliveryPartnerId: deliveryPartnerId,
+  });
+  if (!order) {
+    return next(new CustomError("Order not found", 404));
+  }
+  if (order.orderStatus === "accepted_by_delivery_partner") {
+    return next(new CustomError("Order already accepted", 400));
+  }
+
+  const findPartner = await DeliveryPartner.findById(deliveryPartnerId);
+  if (!findPartner) {
+    return next(new CustomError("Delivery Partner not found", 404));
+  }
+
+  const findCutomer  = await customerModel.findById(order.customerId);
+  if (!findCutomer) {
+    return next(new CustomError("Customer not found", 404));
+  }
+
+  if (status == "accepted") {
+    order.orderStatus = "accepted_by_delivery_partner";
+    // order.deliveryPartnerId = deliveryPartnerId;
+    order.deliveryPartnerAttempts.push({
+      deliveryPartnerId: deliveryPartnerId,
+      status: "accepted",
+    });
+
+    const pharmacyCoordinates = order.pickupAddress;
+    
+  
+
+    if (pharmacyCoordinates && findPartner.location) {
+        let pharmacyToDeliveryRoute = await getRouteBetweenCoords(
+          pharmacyCoordinates,
+          findPartner.location
+        );
+        if (pharmacyToDeliveryRoute)
+          order.pharmacyToDeliveryPartnerRoute = pharmacyToDeliveryRoute;
+        await order.save();
+      }
+
+      const newNotification = new notificationModel({
+        title: "Order Accepted",
+        message: "Your Order has been accepted by delivery partner",
+        recipientType: "customer",
+        notificationType: "pickup_request",
+        NotificationTypeId: order._id,
+        recipientId: findCutomer._id,
+      });
+
+      await newNotification.save();
+
+      if (findCutomer.deviceToken) {
+        await sendExpoNotification(
+          [findCutomer.deviceToken],
+          "Order Accepted",
+          "Your Order has been accepted by delivery partner",
+          newNotification
+        );
+      }
+
+
+    await order.save();
+    return successRes(res, 200, true, "Order accepted successfully", order);
+  } else if (status == "rejected") {
+    order.deliveryPartnerAttempts.push({
+      deliveryPartnerId: deliveryPartnerId,
+      status: "rejected",
+    });
+
+    await order.save();
+    const availablePartners = await DeliveryPartner.find({
+      availabilityStatus: "available",
+      isBlocked: false,
+      deviceToken: { $ne: null },
+      "location.lat": { $ne: null },
+      "location.long": { $ne: null },
+    });
+    const sortedPartners = availablePartners
+      .map((dp) => ({
+        ...dp._doc,
+        distance: getDistance(pharmacyCoordinates, dp.location),
+      }))
+      .sort((a, b) => a.distance - b.distance);
+
+    if (
+      sortedPartners.length > 0 &&
+      sortedPartners[0]._id != deliveryPartnerId
+    ) {
+      const nearestPartner = sortedPartners[0];
+      order.deliveryPartnerId = nearestPartner._id;
+      order.deliveryPartnerAttempts.push({
+        deliveryPartnerId: nearestPartner._id,
+        status: "pending",
+        attemptedAt: new Date(),
+      });
+
+      order.deliveryPartnerQueue.push(nearestPartner._id);
+
+      const newNotification = new notificationModel({
+        title: "New Pickup Order",
+        message: "You have a new pickup order request",
+        recipientType: "delivery_partner",
+        notificationType: "pickup_request",
+        NotificationTypeId: order._id,
+        recipientId: nearestPartner._id,
+      });
+
+      await newNotification.save();
+
+      if (nearestPartner.deviceToken) {
+        await sendExpoNotification(
+          [nearestPartner.deviceToken],
+          "New Delivery Request",
+          "You have a new delivery request",
+          newNotification
+        );
+      }
+    } else {
+      // No delivery partner available → notify admin
+      const admins = await adminSchema.find({ role: "superadmin" });
+      console.log(admins, "admins");
+      for (let admin of admins) {
+        const notify = new notificationModel({
+          title: "Manual Delivery Partner Assignment Required",
+          message: `No delivery partner available for order ${order._id}`,
+          recipientType: "admin",
+          notificationType: "manual_delivery_assignment",
+          NotificationTypeId: order._id,
+          recipientId: admin._id,
+        });
+        // await notify.save();
+        if (admin.deviceToken) {
+          await sendExpoNotification(
+            [admin.deviceToken],
+            "Manual Assignment Needed",
+            "No delivery partner available for order",
+            notify
+          );
+        }
+      }
     }
-    const order = await ordersModel.findOne({ _id: orderId, deliveryPartnerId: deliveryPartnerId }).select('-orderStatus -pharmacyQueue -deliveryPartnerQueue -pharmacyResponseStatus -pharmacyAttempts -deliveryPartnerAttempts').populate('assignedPharmacyId', 'pharmacyName address phone completeAddress').populate('customerId', 'fullName phoneNumber profilePicture');
-    if (!order) {
-        return next(new CustomError("Order not found", 404));
-    }
-    return successRes(res, 200, true, "Order fetched successfully", order);
-})
+  } else {
+    return next(new CustomError("Invalid status", 400));
+  }
+  order.orderStatus = orderStatus;
+  await order.save();
+  return successRes(res, 200, true, "Order status updated successfully", order);
+});
--- src/controllers/PharmacyOwnerController/pharmacyOrderController.js
+++ src/controllers/PharmacyOwnerController/pharmacyOrderController.js
@@ -8,30 +8,31 @@ const { sendExpoNotification } = require("../../utils/expoNotification");
 const { getDistance } = require("../../utils/helper");
 const notificationModel = require("../../modals/notification.model");
 const adminSchema = require("../../modals/admin.Schema");
-const Pharmacy = require("../../modals/pharmacy.model")
+const Pharmacy = require("../../modals/pharmacy.model");
 
-
-module.exports.getAllAssignedOrder = asyncErrorHandler(async (req, res, next) => {
+module.exports.getAllAssignedOrder = asyncErrorHandler(
+  async (req, res, next) => {
     const adminId = req.admin._id;
     const findPharmacy = await pharmacyModel.findOne({ adminId });
 
     if (!findPharmacy) {
-        return next(new CustomError("Pharmacy not found", 404));
+      return next(new CustomError("Pharmacy not found", 404));
     }
 
     const pharmacyId = findPharmacy._id;
     const orders = await ordersModel.find({
-        pharmacyResponseStatus: "pending",
-        pharmacyAttempts: {
-            $elemMatch: {
-                pharmacyId: pharmacyId,
-                status: "pending"
-            }
-        }
+      pharmacyResponseStatus: "pending",
+      pharmacyAttempts: {
+        $elemMatch: {
+          pharmacyId: pharmacyId,
+          status: "pending",
+        },
+      },
     });
 
     return successRes(res, 200, true, "Orders fetched successfully", orders);
-});
+  }
+);
 
 /**
 module.exports.acceptOrRejectOrder = asyncErrorHandler(async (req, res, next) => {
@@ -184,25 +185,30 @@ module.exports.acceptOrRejectOrder = asyncErrorHandler(async (req, res, next) =>
 });
  */
 
-module.exports.acceptOrRejectOrder = asyncErrorHandler(async (req, res, next) => {
+module.exports.acceptOrRejectOrder = asyncErrorHandler(
+  async (req, res, next) => {
     const { orderId, pharmacyId, status } = req.body;
 
     if (!["accepted", "rejected"].includes(status)) {
-        return next(new CustomError("Invalid status", 400));
+      return next(new CustomError("Invalid status", 400));
     }
 
     const order = await ordersModel.findById(orderId);
     if (!order) return next(new CustomError("Order not found", 404));
 
     if (status === "accepted" && order.orderStatus === "accepted_by_pharmacy") {
-        return next(new CustomError("Order already accepted", 400));
+      return next(new CustomError("Order already accepted", 400));
     }
     if (status === "rejected" && order.orderStatus === "accepted_by_pharmacy") {
-        return next(new CustomError("Order already accepted. Cannot reject now", 400));
+      return next(
+        new CustomError("Order already accepted. Cannot reject now", 400)
+      );
     }
 
     if (order.assignedPharmacyId?.toString() !== pharmacyId) {
-        return next(new CustomError("This pharmacy is not assigned to this order", 403));
+      return next(
+        new CustomError("This pharmacy is not assigned to this order", 403)
+      );
     }
 
     const findPharmacy = await pharmacyModel.findById(pharmacyId);
@@ -210,100 +216,111 @@ module.exports.acceptOrRejectOrder = asyncErrorHandler(async (req, res, next) =>
     const pharmacyCoordinates = findPharmacy.pharmacyCoordinates;
 
     if (!pharmacyCoordinates?.lat || !pharmacyCoordinates?.long) {
-        return next(new CustomError("Pharmacy coordinates are not available", 400));
+      return next(
+        new CustomError("Pharmacy coordinates are not available", 400)
+      );
     }
 
     // Push pharmacy attempt
     order.pharmacyAttempts.push({
-        pharmacyId,
-        status,
-        attemptedAt: new Date()
+      pharmacyId,
+      status,
+      attemptedAt: new Date(),
     });
 
     // ---------------- ACCEPT ----------------
     if (status === "accepted") {
-        order.pharmacyResponseStatus = "accepted";
-        order.orderStatus = "accepted_by_pharmacy";
-        order.assignedPharmacyCoordinates = pharmacyCoordinates;
+      order.pharmacyResponseStatus = "accepted";
+      order.orderStatus = "accepted_by_pharmacy";
+      order.assignedPharmacyCoordinates = pharmacyCoordinates;
 
-        // Clear pharmacyQueue
-        order.pharmacyQueue = [];
+      // Clear pharmacyQueue
+      order.pharmacyQueue = [];
 
-        const availablePartners = await DeliveryPartner.find({
-            availabilityStatus: "available",
-            isBlocked: false,
-            deviceToken: { $ne: null },
-            "location.lat": { $ne: null },
-            "location.long": { $ne: null }
+      const availablePartners = await DeliveryPartner.find({
+        availabilityStatus: "available",
+        isBlocked: false,
+        deviceToken: { $ne: null },
+        "location.lat": { $ne: null },
+        "location.long": { $ne: null },
+      });
+
+      console.log(availablePartners, "availablePartners");
+
+      const sortedPartners = availablePartners
+        .map((dp) => ({
+          ...dp._doc,
+          distance: getDistance(pharmacyCoordinates, dp.location),
+        }))
+        .sort((a, b) => a.distance - b.distance);
+
+      if (sortedPartners.length > 0) {
+        const nearestPartner = sortedPartners[0];
+        order.deliveryPartnerId = nearestPartner._id;
+        order.deliveryPartnerAttempts.push({
+          deliveryPartnerId: nearestPartner._id,
+          status: "pending",
+          attemptedAt: new Date(),
         });
 
-        console.log(availablePartners, "availablePartners")
+        order.deliveryPartnerQueue.push(nearestPartner._id);
 
-        const sortedPartners = availablePartners
-            .map(dp => ({
-                ...dp._doc,
-                distance: getDistance(pharmacyCoordinates, dp.location)
-            }))
-            .sort((a, b) => a.distance - b.distance);
+        const newNotification = new notificationModel({
+          title: "New Pickup Order",
+          message: "You have a new pickup order request",
+          recipientType: "delivery_partner",
+          notificationType: "pickup_request",
+          NotificationTypeId: order._id,
+          recipientId: nearestPartner._id,
+        });
 
-        if (sortedPartners.length > 0) {
-            const nearestPartner = sortedPartners[0];
-            order.deliveryPartnerId = nearestPartner._id;
-            order.deliveryPartnerAttempts.push({
-                deliveryPartnerId: nearestPartner._id,
-                status: "pending",
-                attemptedAt: new Date()
-            });
+        await newNotification.save();
 
-            order.deliveryPartnerQueue.push(nearestPartner._id);
-
-            const newNotification = new notificationModel({
-                title: "New Pickup Order",
-                message: "You have a new pickup order request",
-                recipientType: "delivery_partner",
-                notificationType: "pickup_request",
-                NotificationTypeId: order._id,
-                recipientId: nearestPartner._id
-            });
-
-            await newNotification.save();
-
-            if (nearestPartner.deviceToken) {
-                await sendExpoNotification(
-                    [nearestPartner.deviceToken],
-                    "New Delivery Request",
-                    "You have a new delivery request",
-                    newNotification
-                );
-            }
-        } else {
-            // No delivery partner available → notify admin
-            const admins = await adminSchema.find({ role: "superadmin" });
-            console.log(admins, "admins")
-            for (let admin of admins) {
-                const notify = new notificationModel({
-                    title: "Manual Delivery Partner Assignment Required",
-                    message: `No delivery partner available for order ${order._id}`,
-                    recipientType: "admin",
-                    notificationType: "manual_delivery_assignment",
-                    NotificationTypeId: order._id,
-                    recipientId: admin._id
-                });
-                // await notify.save();
-                if (admin.deviceToken) {
-                    await sendExpoNotification(
-                        [admin.deviceToken],
-                        "Manual Assignment Needed",
-                        "No delivery partner available for order",
-                        notify
-                    );
-                }
-            }
+        if (nearestPartner.deviceToken) {
+          await sendExpoNotification(
+            [nearestPartner.deviceToken],
+            "New Delivery Request",
+            "You have a new delivery request",
+            newNotification
+          );
         }
+        if (findPharmacy.pharmacyCoordinates && order.deliveryAddress.coordinates) {
+          let pharmacyToCustomerRoute = await getRouteBetweenCoords(
+            findPharmacy.pharmacyCoordinates,
+            order.deliveryAddress.coordinates
+          );
+          if (pharmacyToCustomerRoute)
+            order.pharmacyToCustomerRoute = pharmacyToCustomerRoute;
+          newOrder.save();
+        }
+        order.pickupAddress = findPharmacy.pharmacyAddress;
+      } else {
+        // No delivery partner available → notify admin
+        const admins = await adminSchema.find({ role: "superadmin" });
+        console.log(admins, "admins");
+        for (let admin of admins) {
+          const notify = new notificationModel({
+            title: "Manual Delivery Partner Assignment Required",
+            message: `No delivery partner available for order ${order._id}`,
+            recipientType: "admin",
+            notificationType: "manual_delivery_assignment",
+            NotificationTypeId: order._id,
+            recipientId: admin._id,
+          });
+          // await notify.save();
+          if (admin.deviceToken) {
+            await sendExpoNotification(
+              [admin.deviceToken],
+              "Manual Assignment Needed",
+              "No delivery partner available for order",
+              notify
+            );
+          }
+        }
+      }
 
-
-        // await order.save();
-        return successRes(res, 200, true, "Order accepted successfully", order);
+      // await order.save();
+      return successRes(res, 200, true, "Order accepted successfully", order);
     }
 
     // ---------------- REJECT ----------------
@@ -311,58 +328,62 @@ module.exports.acceptOrRejectOrder = asyncErrorHandler(async (req, res, next) =>
     order.assignedPharmacyId = null;
 
     // Remove from pharmacyQueue
-    order.pharmacyQueue = order.pharmacyQueue.filter(id => id.toString() !== pharmacyId);
+    order.pharmacyQueue = order.pharmacyQueue.filter(
+      (id) => id.toString() !== pharmacyId
+    );
 
-    const attemptedPharmacyIds = order.pharmacyAttempts.map(a => a.pharmacyId.toString());
+    const attemptedPharmacyIds = order.pharmacyAttempts.map((a) =>
+      a.pharmacyId.toString()
+    );
 
     // STEP 1: Check in pharmacyQueue who has not yet attempted
     let nextPharmacy = null;
     for (let id of order.pharmacyQueue) {
-        if (!attemptedPharmacyIds.includes(id.toString())) {
-            nextPharmacy = await pharmacyModel.findById(id);
-            break;
-        }
+      if (!attemptedPharmacyIds.includes(id.toString())) {
+        nextPharmacy = await pharmacyModel.findById(id);
+        break;
+      }
     }
 
     // STEP 2: If none found in queue, find fresh pharmacies
     if (!nextPharmacy) {
-        const customerCoords = order.deliveryAddress.coordinates;
+      const customerCoords = order.deliveryAddress.coordinates;
 
-        const remainingPharmacies = await pharmacyModel.find({
-            _id: { $nin: attemptedPharmacyIds },
-            "location.coordinates": { $exists: true }
-        });
+      const remainingPharmacies = await pharmacyModel.find({
+        _id: { $nin: attemptedPharmacyIds },
+        "location.coordinates": { $exists: true },
+      });
 
-        const sortedPharmacies = remainingPharmacies
-            .map(pharmacy => ({
-                ...pharmacy._doc,
-                distance: getDistance(customerCoords, pharmacy.location.coordinates)
-            }))
-            .sort((a, b) => a.distance - b.distance);
+      const sortedPharmacies = remainingPharmacies
+        .map((pharmacy) => ({
+          ...pharmacy._doc,
+          distance: getDistance(customerCoords, pharmacy.location.coordinates),
+        }))
+        .sort((a, b) => a.distance - b.distance);
 
-        if (sortedPharmacies.length > 0) {
-            nextPharmacy = sortedPharmacies[0];
-        }
+      if (sortedPharmacies.length > 0) {
+        nextPharmacy = sortedPharmacies[0];
+      }
     }
 
     if (!nextPharmacy) {
-        await order.save();
+      await order.save();
 
-        const admins = await adminSchema.find({ role: "superadmin" });
+      const admins = await adminSchema.find({ role: "superadmin" });
 
-        for (let admin of admins) {
-            const notify = new notificationModel({
-                title: "Manual Pharmacy Assignment Required",
-                message: `No pharmacy available for order ${order._id}`,
-                recipientType: "admin",
-                notificationType: "manual_pharmacy_assignment",
-                NotificationTypeId: order._id,
-                recipientId: admin._id
-            });
-            await notify.save();
-        }
+      for (let admin of admins) {
+        const notify = new notificationModel({
+          title: "Manual Pharmacy Assignment Required",
+          message: `No pharmacy available for order ${order._id}`,
+          recipientType: "admin",
+          notificationType: "manual_pharmacy_assignment",
+          NotificationTypeId: order._id,
+          recipientId: admin._id,
+        });
+        await notify.save();
+      }
 
-        return successRes(res, 200, true, "Order rejected.", order);
+      return successRes(res, 200, true, "Order rejected.", order);
     }
 
     // Assign to next pharmacy
@@ -373,58 +394,73 @@ module.exports.acceptOrRejectOrder = asyncErrorHandler(async (req, res, next) =>
     await order.save();
 
     if (nextPharmacy.deviceToken) {
-        await sendExpoNotification(
-            [nextPharmacy.deviceToken],
-            "New Order Request",
-            "You have a new pharmacy order request",
-            {
-                path: "PharmacyOrderScreen",
-                orderId: order._id,
-                medicineName: order.items[0]?.medicineName || "Order",
-                orderType: order.orderType
-            }
-        );
+      await sendExpoNotification(
+        [nextPharmacy.deviceToken],
+        "New Order Request",
+        "You have a new pharmacy order request",
+        {
+          path: "PharmacyOrderScreen",
+          orderId: order._id,
+          medicineName: order.items[0]?.medicineName || "Order",
+          orderType: order.orderType,
+        }
+      );
     }
-    return successRes(res, 200, true, "Order rejected and reassigned to another pharmacy", order);
-});
-
-module.exports.getAcceptedOrdersByPharmacy = asyncErrorHandler(async (req, res) => {
-  const admin = req.admin;
-
-  const pharmacy = await Pharmacy.findOne({ adminId: admin._id });
-
-  if (!pharmacy) {
-    return errorRes(res, 404, false, "Pharmacy not found");
+    return successRes(
+      res,
+      200,
+      true,
+      "Order rejected and reassigned to another pharmacy",
+      order
+    );
   }
+);
 
-  const acceptedOrders = await ordersModel
-    .find(
-      {
-        assignedPharmacyId: pharmacy._id,
-        pharmacyResponseStatus: "accepted"
-      },
-      {
-        _id: 1,
-        orderDate: 1,
-        customerId: 1,
-        deliveryPartnerId: 1,
-        items: 1,
-        totalAmount: 1,
-        paymentMethod: 1,
-        paymentStatus: 1,
-        "deliveryAddress.deliveryAddressId": 1,
-        assignedPharmacyId: 1,
-      }
-    )
-    .populate({
-      path: "customerId",
-      select: "fullName email phoneNumber"
-    })
-    .populate({
-      path: "deliveryPartnerId",
-      select: "fullname email location phone"
-    })
-    .sort({ orderDate: -1 });
+module.exports.getAcceptedOrdersByPharmacy = asyncErrorHandler(
+  async (req, res) => {
+    const admin = req.admin;
 
-  return successRes(res, 200, true, "Accepted orders fetched", acceptedOrders);
-});
+    const pharmacy = await Pharmacy.findOne({ adminId: admin._id });
+
+    if (!pharmacy) {
+      return errorRes(res, 404, false, "Pharmacy not found");
+    }
+
+    const acceptedOrders = await ordersModel
+      .find(
+        {
+          assignedPharmacyId: pharmacy._id,
+          pharmacyResponseStatus: "accepted",
+        },
+        {
+          _id: 1,
+          orderDate: 1,
+          customerId: 1,
+          deliveryPartnerId: 1,
+          items: 1,
+          totalAmount: 1,
+          paymentMethod: 1,
+          paymentStatus: 1,
+          "deliveryAddress.deliveryAddressId": 1,
+          assignedPharmacyId: 1,
+        }
+      )
+      .populate({
+        path: "customerId",
+        select: "fullName email phoneNumber",
+      })
+      .populate({
+        path: "deliveryPartnerId",
+        select: "fullname email location phone",
+      })
+      .sort({ orderDate: -1 });
+
+    return successRes(
+      res,
+      200,
+      true,
+      "Accepted orders fetched",
+      acceptedOrders
+    );
+  }
+);
