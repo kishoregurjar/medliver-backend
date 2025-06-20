8e5d3ac6c4d16c1ba9c1958b40102adfc55fb941
Chetangit123 <pchetan839@gmail.com>
2025-06-20 12:23:15 +0530

    changes in get-delivery-partner-payment APIs

--- src/controllers/DeliveryPartnerController/deliveryPartnerPaymentController.js
+++ src/controllers/DeliveryPartnerController/deliveryPartnerPaymentController.js
@@ -3,42 +3,68 @@ const deliveryPartnerPaymentModel = require("../../modals/deliveryPartnerPayment
 const asyncErrorHandler = require("../../utils/asyncErrorHandler");
 const { successRes } = require("../../services/response");
 const CustomError = require("../../utils/customError");
-
 module.exports.getDeliveryPartnerPayment = asyncErrorHandler(async (req, res, next) => {
-    let { paymentStatus, timePeriod } = req.query;
+    let { paymentStatus, timePeriod, year, month } = req.query;
     let deliveryPartnerId = req.partner._id;
 
     if (!paymentStatus) {
         return next(new CustomError("Payment Status is required", 400));
     }
-    if (!timePeriod) {
-        return next(new CustomError("Time Period is required", 400));
-    }
 
     const now = new Date();
     let filter = { paymentStatus, deliveryPartnerId };
 
-    switch (timePeriod.toLowerCase()) {
-        case "1 day":
-            filter.createdAt = { $gte: moment(now).subtract(1, 'days').toDate() };
-            break;
-        case "1 week":
-            filter.createdAt = { $gte: moment(now).subtract(1, 'weeks').toDate() };
-            break;
-        case "1 month":
-            filter.createdAt = { $gte: moment(now).subtract(1, 'months').toDate() };
-            break;
-        case "1 year":
-            filter.createdAt = { $gte: moment(now).subtract(1, 'years').toDate() };
-            break;
-        case "all":
-            // No date filter needed
-            break;
-        default:
-            return next(new CustomError("Invalid time period", 400));
+    // Priority: year/month filters over timePeriod
+    if (year && month) {
+        // Specific month in a specific year
+        const startDate = moment().year(Number(year)).month(Number(month) - 1).startOf("month").toDate();
+        const endDate = moment().year(Number(year)).month(Number(month) - 1).endOf("month").toDate();
+        filter.createdAt = { $gte: startDate, $lte: endDate };
+
+    } else if (year) {
+        // Full year filter
+        const startDate = moment().year(Number(year)).startOf("year").toDate();
+        const endDate = moment().year(Number(year)).endOf("year").toDate();
+        filter.createdAt = { $gte: startDate, $lte: endDate };
+
+    } else {
+        // Use timePeriod if year/month not given
+        switch (timePeriod?.toLowerCase()) {
+            case "1 day":
+                filter.createdAt = { $gte: moment().startOf("day").toDate() };
+                break;
+
+            case "1 week":
+                filter.createdAt = { $gte: moment().startOf("isoWeek").toDate() };
+                break;
+
+            case "1 month":
+                filter.createdAt = { $gte: moment().startOf("month").toDate() };
+                break;
+
+            case "1 year":
+                filter.createdAt = { $gte: moment().startOf("year").toDate() };
+                break;
+
+            case "all":
+                // No filter
+                break;
+
+            default:
+                return next(new CustomError("Invalid time period", 400));
+        }
     }
 
-    let deliveryPartnerPayment = await deliveryPartnerPaymentModel.find(filter).populate("orderId");
+    let deliveryPartnerPayment = await deliveryPartnerPaymentModel
+        .find(filter)
+        .populate("orderId");
 
-    return successRes(res, 200, true, "Delivery Partner Payment fetched successfully", deliveryPartnerPayment);
+    return successRes(
+        res,
+        200,
+        true,
+        "Delivery Partner Payment fetched successfully",
+        deliveryPartnerPayment
+    );
 });
+
